a0de5d1c1278458e6dce2370d8c0ad2c97fdb2f8
Felipe <felipe@qualify.com.br>
2023-07-04 10:12:09 -0300

    mensagem de commit

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_ip" {
+    description = "VPC"
+    type = string
+    default = "0152414AC"
+};
+
+variable "subnet" {
+    description = "Valor da subnet"
+    type = string
+    default = ""
+};
+
